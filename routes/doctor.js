a2b25f7af11e6fcfe7b43b338873bd1caca2726c
gianpieryup <gyupanquisalvatierra@gmail.com>
2020-08-07 12:26:43 -0300

    firebase con power shell

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,10 +1,11 @@
 //---- Conexion a FireBase -----------
 const admin = require('firebase-admin');
 
-//var serviceAccount = require("../hospital-b2ba8-firebase-adminsdk-afcg2-93363cd71f.json");
+var serviceAccount = require("../hospital-b2ba8-firebase-adminsdk-afcg2-93363cd71f.json");
 
 admin.initializeApp({
-    credential: admin.credential.applicationDefault(),
+    credential: admin.credential.cert(serviceAccount),
+    //credential: admin.credential.applicationDefault(),
     databaseURL: 'https://hospital-b2ba8.firebaseio.com/'
 });
 const db = admin.database();
@@ -14,19 +15,23 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/registro', (req, res, next)=>{
-    res.render('registroDoctor');
+    res.render('registroDoctor',{
+        errores: false,
+    });
 });
 
 router.post('/registro', (req, res, next)=>{
     console.log(req.body);
     const newDoctor = {
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
+        name: req.body.name,
+        especialidad: req.body.especialidad,
+        dni: req.body.dni,
         email: req.body.email,
-        phone: req.body.phone
+        phone: req.body.phone,
+        disponibilidad: req.body.disponibilidad,
     }
     db.ref('contacts').push(newDoctor); //La Tabla
-    res.json({"status" : "OK"});
+    res.redirect('/');
 });
   
 
